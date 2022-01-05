e682ad43abaf5331b82a31d3323a515d85e1b729
Danudi <danusilva1997@gmail.com>
2022-01-05 14:27:14 +0530

    initial commit

---
+++ src/main/java/com/bolton/singer_gagetmart/SingerGagetmartApplication.java
@@ -0,0 +1,13 @@
+package com.bolton.singer_gagetmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingerGagetmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingerGagetmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolton/singer_gagetmart/SingerGagetmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolton.singer_gagetmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingerGagetmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
